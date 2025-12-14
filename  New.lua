859e9b0d11fc42d5d69c0e109536038f078d0f5e
900Tomm <trongphunguyen11@gmail.com>
2025-12-14 11:42:53 +0700

    Add files via upload

---
+++  New.lua
@@ -0,0 +1,41 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/TheDarkoneMarcillisePex/Other-Scripts/refs/heads/main/Rivals%20GUI'))()
+       local Window = MakeWindow({
+         Hub = {
+         Title = "Tphu",
+         Animation = "TikTok: BuiNhatThaoLy"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"04072011"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=10590477450",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "Script Rivals"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "Thlyy",
+    Callback = function()
+	  loadstring(game:HttpGet('https://raw.githubusercontent.com/TheDarkoneMarcillisePex/Other-Scripts/refs/heads/main/Rivals%20GUI'))()
+  end
+  })
